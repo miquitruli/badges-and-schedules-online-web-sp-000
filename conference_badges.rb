f40743260fc949648735801a86830230030ddc84
MICAELLA COLOMBO <micaelc@g.clemson.edu>
2020-06-03 23:17:40 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,6 +1,11 @@
 # Write your code here.
 
 def badge_maker(names)
+  puts "Hello, my name is #{name}."
+end
+
+
+
   names = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
   name. each do |name|
     puts "Hello, my name is #{name}."
