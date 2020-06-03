355f66d5be7ee030f75b2dc663ae1201cf3fb4cd
MICAELLA COLOMBO <micaelc@g.clemson.edu>
2020-06-03 23:49:28 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -6,9 +6,15 @@ end
 
 def batch_badge_creator(attendees)
   all_attendee = []
-
   attendees. each do |name|
     all_attendee << "Hello, my name is #{name}."
   end
   return all_attendee
+end
+
+def assign_rooms(speaker)
+  assignments = []
+  speaker. each do |name|
+    assignments << "Hello, #{name}! You'll be assigned to room #{name.each_with_index+1}!"
+  return assignments
 end
