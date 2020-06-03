8f372a38994d69a01ec3f8f291c764bca76d0806
MICAELLA COLOMBO <micaelc@g.clemson.edu>
2020-06-03 23:15:00 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,8 @@
 # Write your code here.
+
+def badge_maker(name)
+  name = %w(Edsger, Ada, Charles, Alan, Grace, Linus, and Matz)
+  name. each do |name|
+    puts "Hello, my name is #{name}."
+  end
+end
