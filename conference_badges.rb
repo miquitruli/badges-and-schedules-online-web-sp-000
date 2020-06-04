87b15afead0a0fadf7f0582df4e3b1259025e309
MICAELLA COLOMBO <micaelc@g.clemson.edu>
2020-06-04 00:10:38 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -14,8 +14,10 @@ end
 
 def assign_rooms(attendees)
   room_assignments = []
+  counter = 1
   attendees. each do |name|
-      room_assignments << "Hello, #{name}! You'll be assigned to room #{name.each_with_index}!"
+      room_assignments << "Hello, #{name}! You'll be assigned to room #{counter}!"
+      counter +=1
   end
   return room_assignments
 end
